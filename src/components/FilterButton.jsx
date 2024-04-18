37d172bad2f5f67b0acf7f6b7957202c71df8a0d
Shashank Ankam <shashankankam01@gmail.com>
2024-04-18 12:51:30 -0400

    Added mark all completed/incomplete functionality

--- src/components/FilterButton.jsx
+++ src/components/FilterButton.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { filterTodos, markAllCompleted } from '../redux/actions';
+import { filterTodos, markAllCompleted, markAllIncomplete } from '../redux/actions';
 
 const FilterButton = () => {
 
     const dispatch = useDispatch();
+    const currentTodos = useSelector((state) => state.todos);
     const currentFilter = useSelector((state) => state.filter);
+
     const handleFilter = (filter) => {
         dispatch(filterTodos(filter))
 
     }
+
+    const allTodosCompleted = currentTodos.every(todo => todo.completed);
+
     return (
         <div className='flex space-x-4 items-center'>
             <select
@@ -20,11 +25,19 @@ const FilterButton = () => {
                 <option value='COMPLETED'>Completed</option>
                 <option value='INCOMPLETE'>Incomplete</option>
             </select>
-            
-            <button
-                className='text-sm px-2 py-1 bg-purple-500 text-white ml-2 rounded'
-                onClick={() => dispatch(markAllCompleted())}
-            >Mark All Complete</button>
+            {
+                !allTodosCompleted ?
+                    (<button
+                        name='markAllCompleted'
+                        className='text-sm px-2 py-1 bg-purple-500 text-white ml-2 rounded'
+                        onClick={() => dispatch(markAllCompleted())}
+                    >Mark All Complete</button>
+                    ) : (<button
+                        name='markAllIncomplete'
+                        className='text-sm px-2 py-1 bg-purple-500 text-white ml-2 rounded'
+                        onClick={() => dispatch(markAllIncomplete())}
+                    >Mark All Incomplete</button>
+                    )}
         </div>
     )
 }
